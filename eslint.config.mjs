836f10284d617fee77dfdd411a28b0272d218c1c
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-09-05 01:17:52 +0300

    Init project

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': ['warn', { ignoreVoid: true }],
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+  },
+};
