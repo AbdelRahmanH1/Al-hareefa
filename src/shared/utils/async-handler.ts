ec72d1fd47fbf5498c64d946e39de06b41b6fbcb
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-09-09 00:07:34 +0300

    update wrap response to accept custom message

--- src/shared/utils/async-handler.ts
+++ src/shared/utils/async-handler.ts
@@ -1,5 +1,21 @@
-export const asyncHandler = (fn: Function) => {
-  return (...args: any[]) => {
-    return Promise.resolve(fn(...args)).catch(args[2]);
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export const asyncHandler = (
+  fn: Function,
+  errorMessage = 'Something went wrong',
+  statusCode: number = HttpStatus.INTERNAL_SERVER_ERROR,
+) => {
+  return async (...args: any[]) => {
+    try {
+      return await fn(...args);
+    } catch (err) {
+      // Forward HttpExceptions directly
+      if (err instanceof HttpException) {
+        throw err;
+      }
+
+      // Otherwise, wrap into HttpException
+      throw new HttpException(errorMessage, statusCode);
+    }
   };
 };
