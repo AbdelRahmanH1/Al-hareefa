d4485dce17189de5070565273d76d2684a7a2918
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-09-12 12:12:47 +0300

    fix: validation to accept json and add rules

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,13 @@ async function bootstrap() {
   };
 
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
