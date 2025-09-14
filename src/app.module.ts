79834fa232d0320d4a9537c3158e2de67f57fe7f
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-09-14 18:45:05 +0300

    fix:remove interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { RedisModule } from './redis/redis.module';
+import { TeamsModule } from './teams/teams.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, RedisModule],
+  imports: [PrismaModule, AuthModule, RedisModule, TeamsModule, AdminModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  //app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
