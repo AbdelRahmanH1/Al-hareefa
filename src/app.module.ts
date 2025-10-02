f114fd716baf1dbbb48b5ca78b6f016640d8ea59
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-10-02 19:57:06 +0300

    chore: update tsconfig.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,13 @@ import { RedisModule } from './redis/redis.module';
 import { TeamsModule } from './modules/teams/teams.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { CompetitionsModule } from './modules/competitions/competitions.module';
+import { ServicesController } from './modules/services/services.controller';
+import { ServicesService } from './modules/services/services.service';
+import { ServicesModule } from './modules/services/services.module';
+import { BookingModule } from './modules/booking/booking.module';
+import { PaymentsService } from './modules/payments/payments.service';
+import { PaymentsController } from './modules/payments/payments.controller';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
   imports: [
@@ -14,8 +21,11 @@ import { CompetitionsModule } from './modules/competitions/competitions.module';
     TeamsModule,
     AdminModule,
     CompetitionsModule,
+    ServicesModule,
+    BookingModule,
+    PaymentsModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [ServicesController, PaymentsController],
+  providers: [ServicesService, PaymentsService],
 })
 export class AppModule {}
