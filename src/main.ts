ad12570834c2627a1a508b191d4f0235b7490dbe
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-10-07 19:43:44 +0300

    fix:remove all logs

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,20 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger.config';
+import { PrismaClient } from '@prisma/client/extension';
 
 async function bootstrap() {
   (BigInt.prototype as any).toJSON = function () {
     return this.toString();
   };
 
-  const app = await NestFactory.create(AppModule);
+  const prisma = new PrismaClient({
+    log: ['warn', 'error'],
+  });
+
+  const app = await NestFactory.create(AppModule, {
+    logger: ['warn', 'error'],
+  });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
