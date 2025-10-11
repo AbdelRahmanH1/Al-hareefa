349a2ade747554caa761c1eb90ed49263cd64c50
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-10-11 08:33:45 +0300

    fix:remove logger for path /

--- src/shared/middleware/logger.middleware.ts
+++ src/shared/middleware/logger.middleware.ts
@@ -4,6 +4,9 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    if (req.path === '/') {
+      return next();
+    }
     const { method, originalUrl } = req;
     const start = Date.now();
 
